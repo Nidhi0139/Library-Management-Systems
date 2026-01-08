743b67c152ad356fffb4f4545247f93c0bb08079
Srinidhi <cheemarla.srinidhi2023@vitstudent.ac.in>
2026-01-08 21:56:45 +0530

    merge

--- main0139.py
+++ main0139.py
@@ -1,6 +1,7 @@
 def register(name):
     print(f"(name) has been registered.")
 
+
 #Login Feature code
 def login(user, password):
     # simple hardcoded credentials
@@ -8,7 +9,7 @@ def login(user, password):
     correct_password = "1234"
 
     if user == correct_user and password == correct_password:
-        print("Login successful")
+        print("Login successful student")
     else:
         print("Login failed")
 register("admin")
